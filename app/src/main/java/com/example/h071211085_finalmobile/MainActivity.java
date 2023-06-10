66820eea8b227d38db3cd7af470096340f58c6f3
Hafieddz <hasmy41@gmail.com>
2023-06-10 21:49:51 +0800

    feat : create fragment for movie and tv show

--- app/src/main/java/com/example/h071211085_finalmobile/MainActivity.java
+++ app/src/main/java/com/example/h071211085_finalmobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.h071211085_finalmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentMovie homeFragment = new FragmentMovie();
+        Fragment fragment =
+                fragmentManager.findFragmentByTag(FragmentMovie.class.getSimpleName());
+        if (!(fragment instanceof FragmentMovie)) {
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_container, homeFragment,
+                            FragmentMovie.class.getSimpleName())
+                    .commit();
+        }
     }
 }
